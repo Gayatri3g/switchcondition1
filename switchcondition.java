06060632e458fa26d9b80a00b860740438acd320
Gayatri3g <155943552+Gayatri3g@users.noreply.github.com>
2024-01-09 08:50:25 +0530

    Update switchcondition1.java

--- switchcondition.java
+++ switchcondition.java
@@ -1,6 +1,6 @@
   
 import java.util.*;
-     class switchcondition11
+     class switchcondition1
      {
 	         public static void main(String[] args) {
 	          Scanner sc=new Scanner(System.in);
